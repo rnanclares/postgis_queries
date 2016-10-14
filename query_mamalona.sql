5df33823f01d420a70d18e61c6d14123a5a269c4
RNanclares <RNanclares@users.noreply.github.com>
2016-10-14 13:39:10 -0500

    Add files via upload

---
+++ query_mamalona.sql
@@ -0,0 +1,7 @@
+﻿SELECT string_agg(replace(replace(replace(ST_AsText(geom),'POINT(',''),')',''),' ',','), ',')
+FROM (
+  SELECT (ST_DumpPoints(g.geom)).*
+  FROM
+    (SELECT ST_EXTENT(ST_buffer(geom,50)) as geom FROM catastro.predios_utm_13n
+	WHERE clavegdl = 'D66A2358031') AS g
+  ) j WHERE path=ARRAY[1, 1] OR path=ARRAY[1,3];
