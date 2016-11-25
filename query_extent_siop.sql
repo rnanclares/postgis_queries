51b6aeb251dab45a414ec3cc438e3b7b680ece35
RNanclares <RNanclares@users.noreply.github.com>
2016-11-25 16:58:25 -0600

    Update query_extent_siop.sql

--- query_extent_siop.sql
+++ query_extent_siop.sql
@@ -7,6 +7,6 @@ FROM (
   ) j WHERE path=ARRAY[1, 1] OR path=ARRAY[1,3];
   
   
-//http://localhost/geoserver/gdlsig/wms?request=GetMap&layers=gdlsig:predios_utm_13n,gdlsig:red_vial_utm_13n&width=800&height=600&format=image/png&BBOX=675055.213075813,2288751.61023266,675176.19788201,2288865.68751845&SRS=epsg:32613&cql_filter=(clavegdl=%27D66A2358031%27);INCLUDE
+--http://localhost/geoserver/gdlsig/wms?request=GetMap&layers=gdlsig:predios_utm_13n,gdlsig:red_vial_utm_13n&width=800&height=600&format=image/png&BBOX=675055.213075813,2288751.61023266,675176.19788201,2288865.68751845&SRS=epsg:32613&cql_filter=(clavegdl=%27D66A2358031%27);INCLUDE
 
-//http://localhost/geoserver/wms?request=GetMap&layers=leafletvisor:Predios_Catastro,gdlsig:predios_utm_13n,gdlsig:red_vial_utm_13n&width=800&height=600&format=image/png&BBOX=675055.213075813,2288751.61023266,675176.19788201,2288865.68751845&SRS=epsg:32613&cql_filter=INCLUDE;(clavegdl=%27D66A2358031%27);INCLUDE
+--//http://localhost/geoserver/wms?request=GetMap&layers=leafletvisor:Predios_Catastro,gdlsig:predios_utm_13n,gdlsig:red_vial_utm_13n&width=800&height=600&format=image/png&BBOX=675055.213075813,2288751.61023266,675176.19788201,2288865.68751845&SRS=epsg:32613&cql_filter=INCLUDE;(clavegdl=%27D66A2358031%27);INCLUDE
