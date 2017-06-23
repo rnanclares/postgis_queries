4c9d99a3c8e3832450f6a393c6f6f4b5bc04d012
RNanclares <RNanclares@users.noreply.github.com>
2017-06-23 13:49:01 -0500

    Create query_vista_reportes_ciudapp.sql

---
+++ query_vista_reportes_ciudapp.sql
@@ -0,0 +1,20 @@
+﻿CREATE OR REPLACE VIEW newbulk.ciudapp_reportes AS 
+ SELECT request._id,
+    request.address_string,
+    request.deleted_at,
+    request.description,
+    request.requested_datetime,
+    request.service,
+    request.service_request_id,
+    request.status,
+    request.device_type,
+    request.updated_datetime,
+    request."position",
+    service_categoria.id_categoria,
+    service_categoria.service_categoria,
+    service.service_name,
+    service.description AS service_description,
+    service.service_code
+   FROM newbulk.request
+     JOIN newbulk.service ON request.service::text = service._id::text
+     LEFT JOIN newbulk.service_categoria ON service._id::text = service_categoria._id::text;
