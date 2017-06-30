2dcdc4c91cd9de4d99d981d244f5bf0665d22cfd
RNanclares <RNanclares@users.noreply.github.com>
2017-06-30 10:48:44 -0500

    Update id_secuencia_con_dos_atributos.sql

--- id_secuencia_con_dos_atributos.sql
+++ id_secuencia_con_dos_atributos.sql
@@ -1,4 +1,4 @@
 CREATE MATERIALIZED VIEW caabsa2julio AS
 SELECT *, row_number() OVER (PARTITION BY id 
-ORDER BY "fecha_y_hora") AS orden
+                             ORDER BY "fecha_y_hora") AS orden
 FROM   basura.caabsa_20170602;
