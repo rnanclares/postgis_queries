7e4bbf565b3fdf8a19b79e7c3a16a4649952c710
rnanclares <RNanclares@users.noreply.github.com>
2017-09-21 09:42:52 -0500

    Update split_lines_by_segment.sql

--- split_lines_by_segment.sql
+++ split_lines_by_segment.sql
@@ -1,5 +1,7 @@
-WITH segments AS (
+WITH segments AS ( 
 SELECT gid, clavegdl, ST_MakeLine(lag((pt).geom, 1, NULL) OVER (PARTITION BY gid ORDER BY gid, (pt).path), (pt).geom) AS geom
 	FROM (SELECT gid, clavegdl, ST_DumpPoints(geom) AS pt FROM anchos.predios_test_lineas) as dumps
     )
-SELECT row_number() over (order by segments.gid) as gid_pkey, * FROM segments WHERE geom IS NOT NULL;
+SELECT row_number() over (order by segments.gid) as gid_pkey,*,degrees(st_azimuth(st_startpoint(geom),st_endpoint(geom))) as azim 
+FROM segments 
+WHERE geom IS NOT NULL;
