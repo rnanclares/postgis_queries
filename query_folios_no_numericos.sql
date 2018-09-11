5ff235c4a4a1166649492d2990f95479362c722a
rnanclares <RNanclares@users.noreply.github.com>
2018-09-11 09:27:54 -0500

    Add files via upload
    Seleccionar inmuebles cuyo folio no es númerico.

---
+++ query_folios_no_numericos.sql
@@ -0,0 +1,19 @@
+﻿SELECT *
+FROM ordenamiento."PatrimonioLevantamiento"
+WHERE "zona" = 'Santuario' AND "folio" != '148\P' AND "folio" != '27\P' AND "folio" != '760\P'
+ORDER BY "folio"::int desc ;
+
+
+SELECT *
+FROM ordenamiento."PatrimonioLevantamiento"
+WHERE "folio" = '148\P';
+
+SELECT *
+FROM ordenamiento."PatrimonioLevantamiento"
+WHERE "folio" = '148\P';
+
+SELECT *
+FROM ordenamiento."PatrimonioLevantamiento"
+WHERE "folio" not in (SELECT "folio" FROM ordenamiento."PatrimonioLevantamiento" WHERE "folio" ~ '^[0-9\.]+$');
+
+select '12.41212' ~ '^[0-9\.]+$'
