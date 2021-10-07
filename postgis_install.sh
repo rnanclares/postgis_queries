ef9e8292e24038b3e948542f046ff0d66d0da5e4
Raul Nanclares <11431207+rnanclares@users.noreply.github.com>
2021-10-07 10:39:24 -0500

    Create postgis_install.sh

---
+++ postgis_install.sh
@@ -0,0 +1,3 @@
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt install postgresql-13 postgresql-13-contrib postgresql-13-postgis-3 postgresql-13-scripts
